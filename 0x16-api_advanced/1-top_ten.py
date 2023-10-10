197adef35f5785c551e61d4889fdd47858bab077
Grace_ngigi <gracengigi859@gmail.com>
2023-10-10 13:31:03 -0700

    top ten titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+"""
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    headers = {"user-Agent": "MyRedditBot/1.0 someone_interesting"}
+    res = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res.status_code == 200:
+        data = res.json()
+        posts = data.get('data', {}).get('children', [])
+
+        if posts:
+            for post in posts:
+                title = post['data']['title']
+                print(title)
+        else:
+                return None
+    elif res.status_code == 302:
+        return None
+    else:
+        return None
