e5697ccfe82a42c0b98bbdc87abb1574dbb21a04
Grace_ngigi <gracengigi859@gmail.com>
2023-10-21 15:40:28 -0700

    correct puppet-lint error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
 # reducing the amount of Nginx server failed requests to 0
-
 # Increase the limit of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
@@ -8,6 +7,6 @@ exec { 'fix--for-nginx':
 
 # Restart Nginx service
 exec { 'nginx-restart':
-  command => 'service nginx restart',
+  command => 'nginx restart',
   path    => '/etc/init.d/'
 }
